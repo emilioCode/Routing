a082e7c5dd069b6b221979a39d79d7caf75a666d
emilioCode <emilio_mem@hotmail.com>
2021-09-03 11:56:52 -0400

    1.Router setup and configuration. 2.genering the components for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { ResetComponent } from './component/reset/reset.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
+const appRoutes: Routes=[
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'reset-password',component: ResetComponent},
+  {path:'**',component: NotFoundComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResetComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
